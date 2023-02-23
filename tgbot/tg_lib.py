99ad4094443e9c9d721e1a0943ed67ce26c3345c
killerarz <milov52@gmail.com>
2023-02-23 18:08:29 +0300

    added registrations

--- tgbot/tg_lib.py
+++ tgbot/tg_lib.py
@@ -1,33 +1,18 @@
 import textwrap
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from .models import RegistrationRequest
 
 def show_auth_keyboard(update, context):
     message = textwrap.dedent('''
         Перед началом использования необходимо зарегистрироваться.
         Пожалуйста, выберите свою роль:''')
 
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("🔐 Получить доступ клиента", callback_data='client_auth')],
-         [InlineKeyboardButton("🔐 Получить доступ исполнителя", callback_data='contracter_auth')], ],
+    reply_markup = ReplyKeyboardMarkup(
+        [[KeyboardButton("🔐 Получить доступ клиента")],
+         [KeyboardButton("🔐 Получить доступ исполнитель")], ],
         resize_keyboard=True
     )
     update.message.reply_text(text=message, reply_markup=reply_markup)
 
 
-def show_registration_keyboard(update, context):
-    query = update.callback_query
-    if query.data == 'client_auth':
-        message = textwrap.dedent('''
-            Напишите название вашей компании и контакную информацию
-            В ближайшее время наш менеджер свяжется с Вами''')
-    else:
-        message = textwrap.dedent('''
-            Напишите немного о себе (резюме)
-            В ближайшее время наш менеджер свяжется с Вами''')
-
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("✅ Отправить заявку", callback_data='send_request')]],
-    )
-    update.callback_query.message.reply_text(text=message, reply_markup=reply_markup)
--- tgbot/tg_support.py
+++ tgbot/tg_support.py
@@ -1,9 +1,11 @@
+import textwrap
+
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
 from telegram.ext import Filters, Updater
 
-from .tg_lib import show_auth_keyboard, show_registration_keyboard
+from .models import ChatState, RegistrationRequest
+from .tg_lib import show_auth_keyboard
 
-from .models import ChatState
 
 def start(update, context):
     show_auth_keyboard(update, context)
@@ -11,8 +13,33 @@ def start(update, context):
 
 
 def handle_auth(update, context):
-    show_registration_keyboard(update, context)
-    return 'HANDLE_NEW_USER'
+    partner_role = RegistrationRequest.CLIENT
+    if 'исполнитель' in update.message.text:
+        partner_role = RegistrationRequest.SUBCONTRACTOR
+    if '🔐' in update.message.text:
+        update.message.reply_text('Название вашей организации')
+        context.user_data['name'] = '1'
+        return "HANDLE_AUTH"
+    elif context.user_data['name'] == '1':
+        context.user_data['name'] = update.message.text
+        update.message.reply_text('Краткая информация о вас')
+        context.user_data['description'] = '2'
+        return "HANDLE_AUTH"
+    elif context.user_data['description'] == '2':
+        context.user_data['description'] = update.message.text
+        RegistrationRequest.objects.create(
+            name=context.user_data['name'],
+            description= context.user_data['description'],
+            partner_role=partner_role,
+            telegram_user_id=update.message.chat_id
+        )
+
+        message = textwrap.dedent('''
+            Спасибо за вашу заявку
+            В ближайшее время с вами свяжется наш менеджер''')
+
+        update.message.reply_text(text=message)
+        return 'HANDLE_NEW_USER'
 
 
 class TgSupportBot(object):
@@ -35,13 +62,12 @@ class TgSupportBot(object):
         else:
             return
 
-        chat_state,_ = ChatState.objects.get_or_create(chat_id=chat_id)
+        chat_state, _ = ChatState.objects.get_or_create(chat_id=chat_id)
 
         if user_reply == '/start':
             user_state = 'START'
         else:
             user_state = chat_state.state
-
 
         state_handler = self.states_functions[user_state]
         try:
